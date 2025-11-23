5b699d0a6fc7df866c2daf38c71a6d7ce68aba86
Leonid Dubinsky <dub@podval.org>
2025-11-22 23:57:23 -0500

    updates

--- .config/yadm/bootstrap.d/dns.sh
+++ .config/yadm/bootstrap.d/dns.sh
@@ -2,3 +2,7 @@
 
 # `drill` DNS tool
 sudo pacman -S --needed ldns
+
+# Enable DHCP search domain (using configuration drop-in)
+sudo mkdir -p /etc/systemd/networkd.conf.d/
+sudo cp ./etc/systemd/networkd.conf.d/80-UseDomains.conf /etc/systemd/networkd.conf.d/
--- .config/yadm/bootstrap.d/keyboards.sh
+++ .config/yadm/bootstrap.d/keyboards.sh
@@ -13,10 +13,10 @@ curl -fsSL https://oryx.nyc3.cdn.digitaloceanspaces.com/keymapp/keymapp-latest.t
   { echo "Download or extraction failed"; exit 1; }
 
 # install udev rules
-RULES="/etc/udev/rules.d/50-zsa.rules"
-if [ ! -f $RULES ]; then
+if [ ! -f /etc/udev/rules.d/50-zsa.rules ]; then
   echo "Installing ZSA udev rules"
-  sudo cp ./50-zsa.rules $RULES
+  sudo mkdir -p /etc/udev/rules.d/
+  sudo cp ./etc/udev/rules.d/50-zsa.rules /etc/udev/rules.d/
   sudo groupadd plugdev
   sudo usermod -aG plugdev $USER
   sudo udevadm control --reload
