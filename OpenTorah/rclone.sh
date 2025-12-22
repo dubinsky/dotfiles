c26ce79a30e6c9ae38144ff42c0bfd4d27b56470
Leonid Dubinsky <dub@opentorah.org>
2025-12-21 20:40:19 -0500

    updates

--- OpenTorah/rclone.sh
+++ OpenTorah/rclone.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "--- syncing 'opentorah-drive:/alter-rebbe'"
-rclone --checksum --progress sync ~/OpenTorah/DRIVE/alter-rebbe opentorah-drive:/alter-rebbe
+rclone --checksum --progress sync ~/OpenTorah/alter-rebbe opentorah-drive:/alter-rebbe
 
 echo "--- syncing 'opentorah-buckets:facsimiles.alter-rebbe.org'"
-rclone --checksum --progress sync ~/OpenTorah/BUCKETS/facsimiles.alter-rebbe.org/ opentorah-buckets:facsimiles.alter-rebbe.org
+rclone --checksum --progress sync ~/OpenTorah/facsimiles.alter-rebbe.org/ opentorah-buckets:facsimiles.alter-rebbe.org
